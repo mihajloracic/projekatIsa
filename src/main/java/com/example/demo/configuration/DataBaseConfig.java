56466b08565982ddf7f1cf787b7d8375469092ad
stevanmatovic <stevan.matovic95@gmail.com>
2017-12-26 15:43:18 +0100

    sql test

---
+++ src/main/java/com/example/demo/configuration/DataBaseConfig.java
@@ -0,0 +1,21 @@
+package com.example.demo.configuration;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataBaseConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+}
